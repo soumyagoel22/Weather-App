a46484183abfc662fdcf691ba6d19a83d89c0ec2
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-09-26 09:29:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+import { fetchWeather } from "./api/FetchWeather";
+import "./App.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      setWeather(data);
+      setQuery("");
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            <sup>&deg;C</sup>
+          </div>
+          <div className="info">
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            <p>{weather.weather[0].description}</p>
+          </div>
+          <div
+            style={{ display: "flex", flexDirection: "row", fontSize: "15px" }}
+          >
+            <div style={{ padding: "3px", margin: "3px" }}>
+              Min: {Math.round(weather.main.temp_min)}
+              <sup>&deg;C</sup>
+            </div>
+            <div style={{ padding: "3px", margin: "3px" }}>
+              Max: {Math.round(weather.main.temp_max)}
+              <sup>&deg;C</sup>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
